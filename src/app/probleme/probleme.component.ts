fa0a0bb22e64e5455a3bf0510a36635d7ca95a0e
lng105 <lng105@outlook.com>
2023-03-16 13:28:21 -0400

    Tp6

---
+++ src/app/probleme/probleme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Inter-probleme',
+  templateUrl: './probleme.component.html',
+  styleUrls: ['./probleme.component.css']
+})
+export class ProblemeComponent {
+
+}
